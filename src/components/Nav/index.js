01a5b443af0632563a298bcf644a81eb26cb1b64
JohnG <johniskandar@gotmail.com>
2021-03-08 20:17:11 -0500

    amending the navbar

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -9,30 +9,29 @@ function Nav(props) {
   } = props;
 
   return (
-    <nav className="navbar navbar-dark bg-dark navbar-expand-lg navbar-light bg-light">
-      <div className="container-fluid">
-      <img src={coverImage} className="rounded-circle border border-5 img-s" alt="..."></img>
-        <a className="navbar-brand ms-3" href="/">John George</a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item">
-              <a className={`nav-link  ${currentNav === "About" && "navActive"}`} aria-current="page" href="#about" onClick={() => setCurrentNav("About")}>
+    <nav className="bg-gray-800 text-white shadow-lg p-6">
+      {/* <img src={coverImage} className="rounded-circle border border-5 img-s" alt="..."></img> */}
+      <div className="container mx-auto">
+        <div className="sm:flex justify-around">
+          <a className="text-6x1 font-bold" href="/">JOHNGEORGE</a>
+
+          {/* Menu Items */}
+          <ul className="sm:selc-center text-xl border-t sm:border-none text-gray-400">
+            <li className="sm:inline-block">
+              <a className="p-3 hover:text-white" aria-current="page" href="#about" onClick={() => setCurrentNav("About")}>
                 About Me</a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-              <span onClick={() => setCurrentNav("Portfolio")}>Portfolio</span>
+            <li className="sm:inline-block">
+              <a className="p-3 hover:text-white" href="#">
+                <span onClick={() => setCurrentNav("Portfolio")}>Portfolio</span>
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">
-              <span onClick={() => setCurrentNav("Contact")}>Contact</span></a>
+            <li className="sm:inline-block">
+              <a className="p-3 hover:text-white" href="#">
+                <span onClick={() => setCurrentNav("Contact")}>Contact</span></a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#" onClick={() => setCurrentNav("Resume")}>Resume</a>
+            <li className="sm:inline-block">
+              <a className="p-3 hover:text-white" href="#" onClick={() => setCurrentNav("Resume")}>Resume</a>
             </li>
           </ul>
         </div>
